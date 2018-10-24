8ac70463b52d840b5550051079571ac54c4b7dff
Mike <lsxxxxxx@126.com>
2018-10-24 11:40:57 +0800

    181024-添加注释

--- source-code/StringCT-java/src/how/monero/hodl/crypto/Curve25519Point.java
+++ source-code/StringCT-java/src/how/monero/hodl/crypto/Curve25519Point.java
@@ -29,7 +29,7 @@ public class Curve25519Point {
     }
   }
 
-  public ECPoint point;
+  public ECPoint point; // 这个是主要的数据成员，表示椭圆曲线上的一个点
   public Curve25519Point(ECPoint point) {
     this.point = point;
   }
@@ -37,9 +37,12 @@ public class Curve25519Point {
     this.point = ecsp.getCurve().decodePoint(a);
   }
 
+  // 生成随机点。使用生成点BASE_POINT乘以一个随机标量Scalar.randomScalar()
   public static Curve25519Point randomPoint() {
     return BASE_POINT.scalarMultiply(Scalar.randomScalar());
   }
+  
+  // 点point乘以一个标量，即point*a。这个a往往是私密的
   public Curve25519Point scalarMultiply(Scalar a) {
     scalarMults++;
     if(this==BASE_POINT) scalarBaseMults++;
@@ -55,21 +58,29 @@ public class Curve25519Point {
 
     return new Curve25519Point(point.multiply(a.toBigInteger()));
   }
+  
+  // 点point加另一个点a
   public Curve25519Point add(Curve25519Point a) {
     return new Curve25519Point(point.add(a.point));
   }
+  
+  // 点point减另一个点a
   public Curve25519Point subtract(Curve25519Point a) {
     return new Curve25519Point(point.subtract(a.point));
   }
+  
   public byte[] toBytes() {
     return point.getEncoded(true);
   }
+  
   public boolean satisfiesCurveEquation() {
     return true;
   }
+  
   public static Curve25519Point hashToPoint(byte[] a) {
     return BASE_POINT.scalarMultiply(hashToScalar(a));
   }
+  
   public static Curve25519Point hashToPoint(Curve25519Point a) {
     return hashToPoint(a.toBytes());
   }
@@ -79,7 +90,9 @@ public class Curve25519Point {
     return bytesToHex(toBytes());
   }
 
+  // 获得椭圆曲线的零点，也就是无限的那个点。
   public static Curve25519Point ZERO = new Curve25519Point(ecsp.getCurve().getInfinity());
+  // 获得椭圆曲线的生成点BASE_POINT
   public static Curve25519Point BASE_POINT = new Curve25519Point(ecsp.getG());
   public static Curve25519Point G = BASE_POINT;
 
